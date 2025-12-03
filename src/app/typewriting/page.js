b5e462fb37da06299a968b4b7c98525b602fda00
nishaaanth2 <itismenishaanth@gmail.com>
2025-12-03 20:15:52 +0530

    feat: Add tabbed interface for HTML and CSS code display in TypeWritingPage

--- src/app/typewriting/page.js
+++ src/app/typewriting/page.js
@@ -1,6 +1,14 @@
+"use client"
 import "./style.css";
+import { useState } from "react";
 
 export default function TypeWritingPage() {
+  const [activeTab, setActiveTab] = useState("HTML");
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
     <div className="min-h-screen flex flex-col md:flex-row">
       {/* Left Side - Preview */}
@@ -9,10 +17,23 @@ export default function TypeWritingPage() {
       </div>
 
       {/* Right Side - Code Display */}
-      <div className="flex-1 flex items-center justify-center bg-gray-800 text-green-400 p-6 overflow-y-auto">
-        <pre className="text-sm whitespace-pre-wrap">
+      <div className="flex-1 flex flex-col items-center justify-center bg-gray-800 text-green-400 p-6 overflow-y-auto">
+        <div className="tabs mb-4 bg-white ">
+          <button 
+            className={`tab-button py-2 px-4 ${activeTab === "HTML" ? "active bg-red-400" : ""}`} 
+            onClick={() => handleTabClick("HTML")}
+          >
+            HTML
+          </button>
+          <button 
+            className={`tab-button py-2 px-4 ${activeTab === "CSS" ? "active bg-red-400" : ""}`} 
+            onClick={() => handleTabClick("CSS")}
+          >
+            CSS
+          </button>
+        </div>
+        <pre className={`text-sm whitespace-pre-wrap ${activeTab === "HTML" ? "" : "hidden"}`}>
           {`
-
 **index.html**
 
 <!DOCTYPE html>
@@ -26,26 +47,27 @@ export default function TypeWritingPage() {
   <h2>Hello World!..</h2>
 </body>
 </html>
-
---------------------------------------------
-
+`}
+        </pre>
+        <pre className={`text-sm whitespace-pre-wrap ${activeTab === "CSS" ? "" : "hidden"}`}>
+          {`
 **style.css**
 
 h2 {
-  width: 20ch;             
-  font-family: monospace;  
-  text-wrap: nowrap;
-  overflow: hidden;        
-  animation: typing 2s steps(20) infinite alternate-reverse;
+\twidth: 20ch;             
+  
+\ttext-wrap: nowrap;
+\toverflow: hidden;        
+\tanimation: typing 2s steps(20) infinite alternate-reverse;
 }
 
 @keyframes typing {
-  from {
-    width: 0ch;  
-  }
-  to {
-    width: 20ch; 
-  }
+\tfrom {
+\t\twidth: 0ch;  
+\t}
+\tto {
+\t\twidth: 20ch; 
+\t}
 }
 `}
         </pre>
